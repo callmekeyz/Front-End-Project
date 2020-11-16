2f1ca9d89dab8a821f9d3901a01ea77ed9f9eae9
Sam Hollyer <rsamhollyer@gmail.com>
2020-11-16 16:08:52 -0600

    updated quote, added image modules

--- modules/background-image-module.js
+++ modules/background-image-module.js
@@ -1,6 +1,15 @@
 import { ajax } from "../modules/ajax-module.js";
 
-ajax("https://api.unsplash.com/photos/random", (r) => {
-	let results = JSON.parse(r);
-	console.log(results);
-});
+let randomGeneratedImageID = Math.floor(Math.random() * 101);
+
+export const backgroundImage = ajax(
+	`https://picsum.photos/id/${randomGeneratedImageID}/info`,
+	(r) => {
+		let resultsArray = JSON.parse(r);
+		console.log(resultsArray);
+		const imageSection = document.querySelector(".image-section");
+		const newImage = document.createElement("img");
+		newImage.src = resultsArray.download_url;
+		imageSection.appendChild(newImage);
+	}
+);
--- modules/quote.js
+++ modules/quote.js
@@ -1,38 +1,39 @@
-import { ajax } from "../modules/ajax-module.js"
-const quoteSection = document.querySelector(".quote-section")
-export let newQuote = ajax("https://quote-garden.herokuapp.com/api/v2/quotes/random",(r)=>{
-    let randomQuote = JSON.parse(r)
-    
+import { ajax } from "../modules/ajax-module.js";
 
+const quoteSection = document.querySelector(".quote-section");
 
-   
-        let currentQuote = randomQuote.quote.quoteText
-        let div = document.createElement("div")
-        let p = document.createElement("p")
-        p.setAttribute("id","quote-paragraph")
-        p.append(currentQuote)
-        div.append(p)
-        quoteSection.append(div)
+export let newQuote = () => {
+	ajax("https://quote-garden.herokuapp.com/api/v2/quotes/random", (r) => {
+		let randomQuote = JSON.parse(r);
+		let currentQuote = randomQuote.quote.quoteText;
+		let currentQuoteAuthor = randomQuote.quote.quoteAuthor;
+		appendQuoteToDOM(currentQuote, currentQuoteAuthor);
+	});
+};
+const appendQuoteToDOM = (quote, author) => {
+	const paragraph = document.querySelector("#quote-paragraph");
+	const authorNameId = document.querySelector("#author-name");
 
-        let currentQuoteAuthor = randomQuote.quote.quoteAuthor
+	if (paragraph) {
+		paragraph.innerText = quote;
+		authorNameId.innerText = author || "-unknown";
+		return;
+	}
+	let div = document.createElement("div");
+	let p = document.createElement("p");
+	p.id = "quote-paragraph";
+	p.append(quote);
+	div.append(p);
+	quoteSection.append(div);
 
-        let author = document.createElement("h6")
-        author.setAttribute("id","author-name")
-        if (currentQuoteAuthor !=false){
-        author.append(currentQuoteAuthor)
-        div.append(author)
-        } else{
-            author.append("-unknown")
-            div.append(author)
-        }
-        const refreshButton = document.querySelector(".refresh-quote")
+	let authorEL = document.createElement("h6");
+	authorEL.id = "author-name";
 
-
-refreshButton.addEventListener("click",newQuote)
-// // newQuote
-// console.log("test")
-// };
-
-
-
-})
+	if (author != false) {
+		authorEL.append(author);
+		div.append(authorEL);
+	} else {
+		authorEL.append("-unknown");
+		div.append(authorEL);
+	}
+};
