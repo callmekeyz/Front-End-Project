fd174809389af9448806e2c2dcedd0103668610d
Sam Hollyer <rsamhollyer@gmail.com>
2020-11-16 13:28:03 -0600

    added gradient module and updated clocks

--- modules/clock-module.js
+++ modules/clock-module.js
@@ -6,14 +6,14 @@ let minuteHand = document.querySelector(".minute-hand");
 let hourHand = document.querySelector(".hour-hand");
 let digitalClock = document.querySelector(".digital-clock");
 
-const runClock = () => {
+export const runClock = () => {
 	let now = new Date();
 	let seconds = now.getSeconds();
 	let minutes = now.getMinutes();
 	let hours = now.getHours();
 	let am_pm = "AM";
 
-	if (!analogClockFace.classList.contains("hidden")) {
+	if (!analogClock.classList.contains(".hidden")) {
 		let secondsPercent = seconds / 60;
 		let minutesPercent = (secondsPercent + minutes) / 60;
 		let hoursPercent = (minutesPercent + hours) / 12;
@@ -21,9 +21,8 @@ const runClock = () => {
 		rotateHands(secondHand, secondsPercent);
 		rotateHands(minuteHand, minutesPercent);
 		rotateHands(hourHand, hoursPercent);
-	} else {
-		//Will place digital functionality in here when style setup. ALso need toggle hidden switch setup
 	}
+
 	if (hours > 12) {
 		hours -= 12;
 		am_pm = "PM";
@@ -36,12 +35,9 @@ const runClock = () => {
 	minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
 	seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
 	let displayCurrentTime = `${hours} : ${minutes} : ${seconds} ${am_pm}`;
-	// console.log(displayCurrentTime);
 	digitalClock.innerText = displayCurrentTime;
 };
 
 const rotateHands = (element, rotate) => {
 	element.style.setProperty(`--rotation`, rotate * 360);
 };
-
-setInterval(runClock, 1000);
