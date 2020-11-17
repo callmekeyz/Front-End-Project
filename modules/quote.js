e63b1900cdb7e2605b58196e6c3a9c6526206b77
Sam Hollyer <rsamhollyer@gmail.com>
2020-11-17 09:35:07 -0600

    updated scripts and css

--- modules/quote.js
+++ modules/quote.js
@@ -1,19 +1,22 @@
 import { ajax } from "../modules/ajax-module.js";
 
 const quoteSection = document.querySelector(".quote-section");
-
+//Using a function to make the AJAX call. Need this to hook up the refresh button
 export let newQuote = () => {
 	ajax("https://quote-garden.herokuapp.com/api/v2/quotes/random", (r) => {
 		let randomQuote = JSON.parse(r);
 		let currentQuote = randomQuote.quote.quoteText;
 		let currentQuoteAuthor = randomQuote.quote.quoteAuthor;
-		appendQuoteToDOM(currentQuote, currentQuoteAuthor);
+		appendQuoteToDOM(currentQuote, currentQuoteAuthor); //pass the above variables thru the next function to update the DOM
 	});
 };
+
+//This function handles all the DOM heaving lifting
 const appendQuoteToDOM = (quote, author) => {
 	const paragraph = document.querySelector("#quote-paragraph");
 	const authorNameId = document.querySelector("#author-name");
 
+	//This logic runs the display text of the quote that is generated. It returns out to stop running the code below since that only needs to be run the first time the AJAX is run
 	if (paragraph) {
 		paragraph.innerText = quote;
 		authorNameId.innerText = author || "-unknown";
@@ -29,6 +32,7 @@ const appendQuoteToDOM = (quote, author) => {
 	let authorEL = document.createElement("h6");
 	authorEL.id = "author-name";
 
+	//Need to make sure to account for any missing author value from the JSON
 	if (author != false) {
 		authorEL.append(author);
 		div.append(authorEL);
